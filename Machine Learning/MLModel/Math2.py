414d9e97bc7c00470a4cab2d36655a1c8d4c690f
Omar Belkady <omarbelkady@gmail.com>
2026-03-23 02:40:00 +0100

    new stuff

---
+++ Machine Learning/MLModel/Math2.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return 3*x**2 - 4*x + 5 
+print(f(3.0))
+
+xs = np.arange(-5,5, 0.25)
+ys = f(xs)
+print(xs)
+print(ys)
